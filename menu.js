aabf549f0449dd589d2477e24068cf135171452a
c2ber <73525374+c2ber@users.noreply.github.com>
2026-04-15 19:22:11 +0900

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,27 @@
+const menus = [
+  {
+    name: "김치찌개",
+    place: "홍대 김치집",
+    address: "서울 마포구 홍익로 10"
+  },
+  {
+    name: "삼겹살",
+    place: "강남 삼겹살 맛집",
+    address: "서울 강남구 테헤란로 123"
+  },
+  {
+    name: "비빔밥",
+    place: "전주식당",
+    address: "서울 종로구 종로 1"
+  },
+  {
+    name: "돈까스",
+    place: "명동 돈까스",
+    address: "서울 중구 명동길 25"
+  },
+  {
+    name: "냉면",
+    place: "을지로 평양냉면",
+    address: "서울 중구 을지로 50"
+  }
+];
