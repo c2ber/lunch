29b7c7681ffcd236bec125ebab107d9d5c0955fa
c2ber <73525374+c2ber@users.noreply.github.com>
2026-04-15 20:38:28 +0900

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,44 +1,12 @@
 window.lastUpdate = "2026-04-15";
 
-window.menus = [
-  {
-    main: "삼선짬뽕, 고추간짜장",
-    name: "천자문 우산점",
-    link: "https://place.map.kakao.com/491748732"
-  },
-  {
-    main: "고기짬뽕, 중화비빔밥",
-    name: "보배반점 광주하남2지구점",
-    link: "https://place.map.kakao.com/1658018290"
-  },
-  {
-    main: "양평해장국",
-    name: "일품양평해장국 광주어등산CC점",
-    link: "https://place.map.kakao.com/638703443"
-  },
-  {
-    main: "김치나베, 눈꽃돈까스",
-    name: "토토로키친",
-    link: "https://place.map.kakao.com/1780143123"
-  },
-  {
-    main: "제육, 김치찌개",
-    name: "금호식당",
-    link: "https://place.map.kakao.com/1664448308"
-  },
-  {
-    main: "잔치국수, 보쌈서비스",
-    name: "순자국수,보쌈",
-    link: "https://place.map.kakao.com/1756861545"
-  },
-  {
-    main: "모듬국밥, 후라이서비스",
-    name: "곱창나무수제국밥 광주본점",
-    link: "https://place.map.kakao.com/589954272"
-  },
-  {
-    main: "뼈해장국, 후라이서비스",
-    name: "어등돌솥뼈해장국 하남2호점",
-    link: "https://place.map.kakao.com/166423908"
-  }
-];
+window.rawMenus = `
+중식|삼선짬뽕, 고추간짜장|천자문 우산점|https://place.map.kakao.com/491748732
+중식|고기짬뽕, 중화비빔밥|보배반점 광주하남2지구점|https://place.map.kakao.com/1658018290
+한식|양평해장국|일품양평해장국 광주어등산CC점|https://place.map.kakao.com/638703443
+일식|김치나베, 눈꽃돈까스|토토로키친|https://place.map.kakao.com/1780143123
+한식|제육, 김치찌개|금호식당|https://place.map.kakao.com/1664448308
+한식|잔치국수, 보쌈서비스|순자국수,보쌈|https://place.map.kakao.com/1756861545
+한식|모듬국밥, 후라이서비스|곱창나무수제국밥 광주본점|https://place.map.kakao.com/589954272
+한식|뼈해장국, 후라이서비스|어등돌솥뼈해장국 하남2호점|https://place.map.kakao.com/166423908
+`;
